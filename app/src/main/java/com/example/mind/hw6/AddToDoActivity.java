e7988c87869eedda131b31816f5635d340f910e9
bahmanebazoo@gmail.com <email@example.com>
2018-12-23 10:47:49 +0330

    AddToDo Activity & Fragment created. @String and @Color updated. FloatingButton connected to AddToDoActivity.

---
+++ app/src/main/java/com/example/mind/hw6/AddToDoActivity.java
@@ -0,0 +1,27 @@
+package com.example.mind.hw6;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_do);
+
+
+
+    }
+
+    public static Intent newIntent(Context context){
+
+        Intent intent = new Intent(context, AddToDoActivity.class);
+
+        return intent;
+
+    }
+}
