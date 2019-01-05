3dcf7c9d5f64ea31bd2f801c8722f8aefea4c800
bahmanebazoo@gmail.com <email@example.com>
2019-01-05 23:45:35 +0330

    ToDoList updated for use as specific user

--- app/src/main/java/com/example/mind/hw6/model/ToDoList.java
+++ app/src/main/java/com/example/mind/hw6/model/ToDoList.java
@@ -1,7 +1,5 @@
 package com.example.mind.hw6.model;
 
-import android.widget.CheckBox;
-
 import java.util.Date;
 import java.util.UUID;
 
@@ -10,7 +8,16 @@ public class ToDoList {
     private String mDescription;
     private UUID mUUID;
     private Date mDate;
+    private UUID mUserUUID;
     private boolean mDone;
+
+    public UUID getUserUUID() {
+        return mUserUUID;
+    }
+
+    public void setUserUUID(UUID userUUID) {
+        mUserUUID = userUUID;
+    }
 
     public UUID getUUID() {
         return mUUID;
@@ -51,11 +58,19 @@ public class ToDoList {
     public ToDoList() {
         mUUID = UUID.randomUUID();
         mDate = new Date();
+        mDone = false;
+    }
+
+    public ToDoList(UUID uuid) {
+        mUserUUID = uuid;
+        mUUID = UUID.randomUUID();
+        mDate = new Date();
+        mDone = false;
     }
 
     public ToDoList(String title, String description, boolean done) {
         mUUID = UUID.randomUUID();
-       mDone =done;
+        mDone = done;
         mTitle = title;
         mDescription = description;
         mDate = new Date();
